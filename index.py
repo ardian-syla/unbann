2cf702c52e1d9fbd7b9ac2de92c821fedd24ebfd
Gyxi <113261722+fsociety-digital@users.noreply.github.com>
2022-11-08 03:33:05 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
+TOKEN = 'MTAzOTI1NTg4ODgyNjk5MDcxMw.GksOfW.QKznFDJbDhbwmd3yvVrJyd2eyuyb7ypybGVQdk'
+
+@bot.command()
+async def check(ctx, pre, type):
+    embed =discord.Embed(
+        title= 'New Unbann',
+        description=f'**Username:** {pre}\n**Type:** {type}\n**Status:** Unbanned',
+        color=0x774dea
+    )
+    embed.set_thumbnail(url='https://media.discordapp.net/attachments/930661781498642452/1036703925191057418/tcu.gif')
+    embed.set_footer(text='Programmed By ArdianS', icon_url='https://media.discordapp.net/attachments/930661781498642452/1036703925191057418/tcu.gif')
+
+    await ctx.send(embed=embed)
+    channel = bot.get_channel(1039366436809879593)
+    await channel.send(embed=embed)
+
+bot.run(TOKEN)
